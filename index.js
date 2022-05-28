d9d671ed08492635ae3ebbb9512ea937bfcc73bb
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-05-28 14:14:51 +0700

    Http APIs and Routing

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify([
-      {
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  if (req.url === "/friends") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
         id: 1,
         name: "Kim Bui Linh Chi",
-      },
-      {
-        id: 2,
-        name: "Trieu Ngoc Hai",
-      },
-    ])
-  );
+      })
+    );
+  } else if (req.url === "/messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>'Hello, Chi!!!'</li>");
+    res.write("<li>'What are your thoughts on astronomy?'</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end()
+  }
 });
 
 server.listen(PORT, () => {
